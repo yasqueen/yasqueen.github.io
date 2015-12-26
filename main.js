81c66523697e4c99839d24f01dddb99b69b0a5f3
Ariel Marcus <ariel@spotify.com>
2015-12-26 16:49:07 -0500

    basic styles in progress

---
+++ main.js
@@ -0,0 +1,28 @@
+var eight = {
+  'top_bar': {
+    // half size if true
+    'half_size': false,
+    // degrees element is rotated
+    'rotation': 0
+  },
+  // Will default to full size
+  // and rotation of 0
+  'middle_bar': true,
+  'bottom_bar': false,
+  'top_left': 0,
+  'top_right': 1,
+  'bottom_left': {},
+  'bottom_right': []
+};
+
+var counter = 0;
+
+d3.select(".bitwise").selectAll("div")
+  .data(Object.keys(eight))
+  .enter()
+  .append("div")
+  .attr("class", "bar")
+  .style("height", function(d) {
+    counter += 1;
+    return counter*5+"px";
+  });
